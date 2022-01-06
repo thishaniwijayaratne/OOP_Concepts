5644603dd5cd4c4d924044237457c3a91fd2c633
thishaniwijayaratne <anuradha.thishani@gmail.com>
2022-01-06 15:52:54 +0530

    Updating inheritance

--- Python/Inheritance.py
+++ Python/Inheritance.py
@@ -1,14 +1,49 @@
 class Vehicle:
-    def __init__(self, vehicleType, noOfWheels, noOfDoors, noOfSeats):
-        self.vehicleType = vehicleType
-        self.noOfWheels = noOfWheels
-        self.noOfDoors = noOfDoors
-        self.noOfSeats = noOfSeats
+    def __init__(self, vehicle_type, no_of_wheels, no_of_doors, no_of_seats):
+        self.vehicle_type = vehicle_type
+        self.no_of_wheels = no_of_wheels
+        self.no_of_doors = no_of_doors
+        self.no_of_seats = no_of_seats
 
     def get_info(self):
-        print(self.vehicleType, self.noOfWheels, self.noOfDoors, self.noOfSeats)
+        print("This is a", self.vehicle_type, ".\nIt has", self.no_of_wheels, "wheels.",
+              "\nIt has", self.no_of_doors, "doors,", "and", self.no_of_seats, "seats.\n\n")
+
+    def is_car(self):
+        if self.vehicle_type == "car" or self.vehicle_type == "Car":
+            return True
+        return False
+
+
+class Car(Vehicle):
+    def __init__(self, brand, no_of_wheels, no_of_doors, no_of_seats):
+        self.brand = brand
+        self.no_of_wheels = no_of_wheels
+        self.no_of_doors = no_of_doors
+        self.no_of_seats = no_of_seats
+        self.price = 0
+        self.vehicle_type = 'Car'
+
+    def get_price(self):
+        self.price = car_brand_prices[self.brand]* (self.no_of_wheels*250+self.no_of_doors*150+self.no_of_seats*200)
+        print("Price of this ", type(self).__name__, "is", self.price)
+        return self.price
+
+    def is_car(self):
+        return True
+
+# class Motor_Bike(Vehicle):
+#     def
+
+
 
 
 if __name__ == '__main__':
-    car = Vehicle('car', 4, 4, 5)
-    car.get_info()
+    global car_brand_prices
+    car_brand_prices = {"Ferrari":25, "BMW": 20, "rolls royce":35}
+    Lorry = Vehicle('Lorry', 6, 2, 3)
+    Lorry.get_info()
+
+    Car1 = Car("BMW", 4, 4, 6)
+    Car1.get_info()
+    Car1_cost = Car1.get_price()
